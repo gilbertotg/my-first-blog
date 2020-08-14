fe1924fa59666a6778d31f135aa90c398f4db307
gilbertotg <gilberto.telesforo@gmail.com>
2020-08-14 13:39:25 -0500

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,4 +4,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Post #El punto antes de models indica el directorio actual o la aplicación actual. Ambos, views.py y models.py están en el mismo directorio
 from django.utils import timezone
 
@@ -6,3 +6,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #lista de post publicados ordenados por published_date
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail (request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
